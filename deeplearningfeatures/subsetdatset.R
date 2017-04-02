7aecd5962884890ae12272ff8f3c65028a88e7d8
Pablo <pglez82@gmail.com>
2017-04-02 12:22:21 +0200

    preproces images for h2o

--- deeplearningfeatures/subsetdatset.R
+++ deeplearningfeatures/subsetdatset.R
@@ -30,3 +30,42 @@ testNormalFeatures<-function()
   model<-train(x,y,method="rf", trControl=trainControl(method="cv",number=5))
   save(model,file="results/IFCB_SMALL_NORMALFEAT.RData")
 }
+
+#Get the file names in order to process all the images for deep learning
+computeImageFileNames<-function(IFCB)
+{
+  #Get the year
+  year<-sapply(strsplit(IFCB$Sample,"_"),"[[",2)
+  paths<-vector(length=length(year))
+  for (i in 1:length(year))
+    paths[i]<-paste("../data/",year[i],"/",IFCB$Class[i],"/",IFCB$Sample[i],"_",formatC(IFCB$roi_number[i], width=5, flag="0"),".png",sep="")
+  
+  return (paths)
+}
+
+preprocessImagesForH2O<-function()
+{
+  library(EBImage)
+  library(data.table)
+  library(doMC)
+  registerDoMC(cores = 14)
+  
+  dimen<-64
+  IFCB<-fread('export/IFCB_SMALL.csv')
+  paths<-computeImageFileNames(IFCB)
+  IFCB<-IFCB[1:500,]
+  paths<-paths[1:500]
+  images = foreach(i=1:length(paths),.combine = 'rbind')%dopar%
+  {
+    m<-matrix(0,nrow = dimen,ncol=dimen)
+    image<-readImage(paths[i])
+    image<-resize(image,dimen)
+    startx<-(dimen-dim(image)[1])%/%2+1
+    starty<-(dimen-dim(image)[2])%/%2+1
+    m[startx:(startx+dim(image)[1]-1),starty:(starty+dim(image)[2]-1)]=imageData(image)
+    as.vector(m)
+  }
+  
+  res<-data.table(Class=IFCB$Class,Sample=IFCB$Sample,roi_number=IFCB$roi_number,FunctionalGroup=IFCB$FunctionalGroup,images)
+  fwrite(res,file = "export/IFCB_SMALL_H2O.csv")
+}
