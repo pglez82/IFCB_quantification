99ba7b33311f9758a915d4bb38ff8bb494493de1
Pablo <pglez82@gmail.com>
2017-04-16 19:18:35 +0200

    compute 10000 examples

--- deeplearningfeatures/mxnettest.R
+++ deeplearningfeatures/mxnettest.R
@@ -47,9 +47,9 @@ computeDeepFeatures<-function()
   
   #Hasta que no podamos con todo...
   ###################
-  #set.seed(7)
-  #IFCB<-IFCB[sample(nrow(IFCB),10000),]
-  #fwrite(IFCB,'resnetfeatures/normalfeatures.csv')
+  set.seed(7)
+  IFCB<-IFCB[sample(nrow(IFCB),10000),]
+  fwrite(IFCB,'resnetfeatures/normalfeatures.csv')
   ###################
   
   fileNames<-computeImageFileNames(IFCB)
