e5d4146d18e8ff31b6b601607d015a79083579a4
Pablo <pglez82@gmail.com>
2017-04-17 21:38:24 +0200

    save also image size in deep features

--- deeplearningfeatures/mxnettest.R
+++ deeplearningfeatures/mxnettest.R
@@ -30,6 +30,8 @@ testPredict<-function()
 }
 
 
+model<-"resnet-50"
+
 
 #imagen de prueba para probar. Esto saca los valores de cualquier capa de nuestra red. Ahora solo queda paralelizar y calcularlo en todas las imagenes
 #grabarlo en un csv y probar a entrenar.
@@ -41,24 +43,24 @@ computeDeepFeatures<-function()
   require(itertools)
   require(doMC)
   source('subsetdatset.R')
-  nCores<-10
+  nCores<-12
   registerDoMC(cores = nCores)
-  RESULTS_FILE<-"features/resnet50/deepfeatures.csv"
+  RESULTS_FILE<-paste("features/",model,"/deepfeatures.csv",sep="")
   if (file.exists(RESULTS_FILE)) file.remove(RESULTS_FILE)
   
   IFCB<-fread('export/IFCB_SMALL.csv')
   
   #Hasta que no podamos con todo...
   ###################
-  set.seed(7)
-  IFCB<-IFCB[sample(nrow(IFCB),10000),]
-  fwrite(IFCB,'features/resnet50/normalfeatures.csv')
+  #set.seed(7)
+  #IFCB<-IFCB[sample(nrow(IFCB),10000),]
+  fwrite(IFCB,paste("features/",model,"/normalfeatures.csv",sep=""))
   ###################
   
   fileNames<-computeImageFileNames(IFCB)
   
   #Load model
-  model = mx.model.load("models/resnet50/resnet-50", iteration=0)
+  model = mx.model.load(paste("models/",model,"/",model,sep=""), iteration=0)
   internals <- model$symbol$get.internals()
   outputs<-internals$outputs
   t<-sapply(1:length(outputs), function(x){internals$get.output(x)})
@@ -84,7 +86,7 @@ computeDeepFeatures<-function()
         normed <- preproc.image(im)
         mx.exec.update.arg.arrays(executor, list(data=mx.nd.array(normed)), match.name=TRUE)
         mx.exec.forward(executor, is.train=FALSE)
-        as.array(executor$ref.outputs$flatten0_output)    
+        c(as.array(executor$ref.outputs$flatten0_output),dim(im)[1:2])
       }))
     }
     print("Saving to file...")
@@ -101,11 +103,11 @@ testNormalFeatures<-function()
   library(doMC)
   registerDoMC(cores = 15)
   set.seed(7)
-  IFCB_SMALL<-fread("export/IFCB_SMALL.csv")
+  IFCB_SMALL<-fread(paste("features/",model,"/normalfeatures.csv",sep=""))
   y<-factor(IFCB_SMALL$Class)
   x<-IFCB_SMALL[,c("Class","Sample","OriginalClass","roi_number","FunctionalGroup","Area_over_PerimeterSquared","Area_over_Perimeter","H90_over_Hflip","H90_over_H180","Hflip_over_H180","summedConvexPerimeter_over_Perimeter","rotated_BoundingBox_solidity"):=NULL]
-  model<-train(x,y,method="rf", trControl=trainControl(method="cv",number=10))
-  save(model,file="features/resnet50/NORMAL_MODEL.RData")
+  model<-train(x,y,method="rf", trControl=trainControl(method="cv",number=5))
+  save(model,file=paste("features/",model,"/NORMAL_MODEL.RData",sep=""))
 }
 
 testDeepFeatures<-function()
@@ -113,14 +115,12 @@ testDeepFeatures<-function()
   library(caret)
   library(data.table)
   library(doMC)
-  registerDoMC(cores = 15)
+  registerDoMC(cores = 5)
   set.seed(7)
-  IFCB_SMALL<-fread("features/resnet50/deepfeatures.csv")
+  IFCB_SMALL<-fread(paste("features/",model,"/deepfeatures.csv",sep=""))
   y<-factor(IFCB_SMALL$Class)
   x<-IFCB_SMALL[,c("Class"):=NULL]
-  model<-train(x,y,method="svmLinear", trControl=trainControl(method="cv",number=10))
-  save(model,file="features/resnet50/DEEP_MODEL.RData")
+  rm(IFCB_SMALL)
+  model<-train(x,y,method="svmLinear", trControl=trainControl(method="cv",number=5),tuneGrid=expand.grid(C = c(0.1,1,10)))
+  save(model,file=paste("features/",model,"/DEEP_MODEL.RData",sep=""))
 }
-
-
-
