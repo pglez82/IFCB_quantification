86387d48f7ea169da41860760607af4da372a53a
Pablo <pglez82@gmail.com>
2017-05-16 21:58:25 +0200

    bugs in subsetdataset

--- deeplearningfeatures/subsetdatset.R
+++ deeplearningfeatures/subsetdatset.R
@@ -6,29 +6,40 @@ DS_PATH<-'../export/IFCB.csv'
 SMALL_DS_PATH<-'export/IFCB_SMALL.csv'
 
 #This function allows us to extract a smaller dataset from the IFCB dataset
-#The aim of the function is to test de DNN faster
+#The aim of the function is to test de CNN faster
 extractSmallerDataset<-function()
 {
   library(data.table)
-  #Control the size of the dataset
-  proportion<-0.008
+  minExamplesPerClass<-500
+  
   IFCB<-fread(file = DS_PATH)
-  IFCB_SMALL<-IFCB[, if(.N>1000) .SD, by = Class]
+  IFCB_ORIGINAL_CLASS<-readRDS('../IFCB.RData') #We need this for the images
+  IFCB$OriginalClass <- IFCB_ORIGINAL_CLASS$OriginalClass
+  #We will only use for training the CNN the examples used for training in our quantification algorithms
+  training<-read.table('training_samples.csv')
+  IFCB<-IFCB[IFCB$Sample %in% training$V1,]
+  IFCB$Class<-factor(IFCB$Class)
+  IFCB_SMALL<-IFCB[, if(.N>minExamplesPerClass) .SD, by = Class]
   IFCB_SMALL$Class<-factor(IFCB_SMALL$Class)
   
   #Create balanced dataset
-  createSets <- function(x, y, p){
-    nr <- NROW(x)
-    size <- (nr * p) %/% length(unique(y))
-    idx <- lapply(split(seq_len(nr), y), function(.x) sample(.x, size))
+  createSets <- function(x, y, num_examples){
+    size <- num_examples %/% length(unique(y))
+    idx <- lapply(split(seq_len(nrow(x)), y), 
+                  function(.x)
+                  {
+                    if (length(.x)<=size)
+                      .x
+                    else
+                      sample(.x, size)
+                  })
     unlist(idx)
   }
+  
   set.seed(7)
-  ind <- createSets(IFCB_SMALL, IFCB_SMALL$Class, proportion)
-  #We need the detailed class in order to get the image
-  IFCB<-readRDS('../IFCB.RData')
+  ind <- createSets(IFCB_SMALL, IFCB_SMALL$Class, 50000)
+  
   IFCB_SMALL <- IFCB_SMALL[ind,]
-  IFCB_SMALL$OriginalClass <- IFCB$OriginalClass[ind]
   fwrite(IFCB_SMALL,SMALL_DS_PATH,nThread=12)
 }
 
